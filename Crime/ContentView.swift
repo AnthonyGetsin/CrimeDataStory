9edc88c84663642ecf18fdd8a68f6e34cd67ae9d
Anthony Getsin <anthony.getsin@gmail.com>
2025-04-04 18:27:14 -0700

    Initial Commit

---
+++ Crime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crime
+//
+//  Created by Tony on 4/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crime/CrimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrimeApp.swift
+//  Crime
+//
+//  Created by Tony on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CrimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
